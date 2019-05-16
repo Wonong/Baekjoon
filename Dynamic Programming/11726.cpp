338de6e013d5c5aafa6d89c2288428e011c03130
Wonyeong Choi <ciwnyg0815@gmail.com>
2019-05-16 12:48:59 +0900

    11726

---
+++ Dynamic Programming/11726.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int getAns(int *dp, int num);
+
+int main(){
+
+    int n, *dp;
+
+    cin >> n;
+
+    dp = (int*)calloc(n+1, sizeof(int));
+
+    dp[1] = 1;
+    if(n >=2) dp[2] = 2;   
+
+    cout << getAns(dp, n) << endl;
+
+    return 0;
+}
+
+int getAns(int *dp, int num){
+
+    if(dp[num] > 0) return dp[num];
+
+    dp[num] = (getAns(dp, num-1) + getAns(dp, num-2))%10007;
+
+    return dp[num];
+    
+}
+
